dd12c6dfa3d9bef194de9741e88709e8d3f1ce84
Joo <kh.ochir24@gmail.com>
2022-10-28 13:27:36 +0800

    anhnii tulhelt

--- register.php
+++ register.php
@@ -7,36 +7,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
-   <?php include 'registration.php'; ?>
+   <?php //include 'registration.php'; ?>
    <?php include 'navbar.php';?>
    <div class="container">
-   <form action="register.php" method="POST">
-     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Name</label>
-        <input type="text" required name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-     </div>
-     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Username</label>
-        <input type="text" required name="Username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-     </div>
-     <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" required name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-     </div>
-     <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" name="password" required class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password Confirmation</label>
-        <input type="password" name="password_confirmation" required class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-   </form>
+      <?php
+      if(isset($_GET['error'])) {
+         echo $_GET['error'];
+      }
+      ?>
+      <form action="registration.php" method="POST">
+      <div class="mb-3">
+         <label for="exampleInputEmail1" class="form-label">Name</label>
+         <input value="" type="text" required name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+         <label for="exampleInputEmail1" class="form-label">Username</label>
+         <input value="" type="text" required name="Username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+         <label for="exampleInputEmail1" class="form-label">Email address</label>
+         <input value="" type="email" required name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+         <label for="exampleInputPassword1" class="form-label">Password</label>
+         <input type="password" name="password" required class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3">
+         <label for="exampleInputPassword1" class="form-label">Password Confirmation</label>
+         <input type="password" name="password_confirmation" required class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+         <input required type="checkbox" class="form-check-input" id="exampleCheck1">
+         <label class="form-check-label" for="exampleCheck1">Terms & Agreements</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
  </div>
   </body>
--- registration.php
+++ registration.php
@@ -11,12 +11,29 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Amjilttai xD";
 
 
 
+function hoosonbish($field) {
+  if($field != "") {
+    return true;
+    
+  } return false;
+}
 
 if(isset($_POST['email'])) {
-    print_r($_POST);
+    foreach($_POST as $index => $data) {
+      if(hoosonbish($data) == false) {
+        die('hooson baina' . $index);
+      }
+      if($_POST['password'] != $_POST['password_confirmation']) {
+        header('Location: /register.php?error=confirmation');
+      }
+      die('ready');
+    }
+
+
+} else{
+  header("Location: /register.php?");
 }
 ?>
