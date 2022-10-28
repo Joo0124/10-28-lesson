1a5b82d521c975c41e90953d8e5d7160912fa441
Joo <kh.ochir24@gmail.com>
2022-10-28 11:59:11 +0800

    anhnii push

---
+++ registration.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "main";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+
+
+if(isset($_POST['email'])) {
+    print_r($_POST);
+}
+?>
